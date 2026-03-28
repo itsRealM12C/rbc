f79a365c0aca62ee29979d21ef6cc72adc394e8d
itsRealM13C <testelek212@gmail.com>
2026-03-28 13:34:58 +0100

    Update beanbird.sh

--- beanbird.sh
+++ beanbird.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/sh
 # beanbird.sh
 
-# 1. Download
-/usr/bin/curl -L https://github.com/itsRealM12C/convert/raw/refs/heads/main/4.0.zip -o /tmp/4.0.zip
+# 1. Download Beanbird package
+curl -L https://github.com/itsRealM12C/convert/raw/refs/heads/main/4.0.zip -o /tmp/4.0.zip
 
-# 2. Extract
-/usr/bin/unzip -o /tmp/4.0.zip -d /tmp/
-/bin/rm -rf /tmp/cursors
-/bin/mv /tmp/4.0 /tmp/cursors
+# 2. Extract and Prepare
+unzip -o /tmp/4.0.zip -d /tmp/
+rm -rf /tmp/cursors
+mv /tmp/4.0 /tmp/cursors
 
-# 3. Mount
-/bin/mount --bind /tmp/cursors /usr/share/im
+# 3. Apply bind mount
+mount --bind /tmp/cursors /usr/share/im
 
-# 4. Restart UI
-/sbin/initctl restart surface-manager
+# 4. Restart the UI
+initctl restart surface-manager
